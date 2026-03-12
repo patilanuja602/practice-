a219ba5014473579002f4aa94563f9a1783ce0b3
Patil Anuja <anujap602@gmail.com>
2026-03-12 16:07:33 +0530

    before rebase in main

--- Board.py
+++ Board.py
@@ -7,3 +7,6 @@ print("i am at branch 1 before branching")
 
 print("Hi i am in branch1 after the merge")
 print("i am in main after the branch1 merge")
+
+
+print("\\\\\\\\\\\\\\\\\\\\rebasing i am in main\\\\\\\\\\\\\\")
